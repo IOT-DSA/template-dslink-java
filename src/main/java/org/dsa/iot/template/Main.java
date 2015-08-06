d95222f4e33f63abb4d2b127dd96c2e4d6f2fa17
samrg472 <samrg472@gmail.com>
2015-08-06 13:03:04 -0700

    Update to the latest SDK

--- src/main/java/org/dsa/iot/template/Main.java
+++ src/main/java/org/dsa/iot/template/Main.java
@@ -4,29 +4,43 @@ import org.dsa.iot.dslink.DSLink;
 import org.dsa.iot.dslink.DSLinkFactory;
 import org.dsa.iot.dslink.DSLinkHandler;
 import org.dsa.iot.dslink.node.Node;
+import org.dsa.iot.dslink.node.NodeBuilder;
 import org.dsa.iot.dslink.node.value.Value;
+import org.dsa.iot.dslink.node.value.ValueType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author Logan Gorence
+ * The main class that starts the DSLink. Typically it extends
+ * {@link DSLinkHandler} and the main method extends into it.
  */
 public class Main extends DSLinkHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        DSLinkFactory.startResponder("Template", args, new Main());
+    @Override
+    public boolean isResponder() {
+        return true;
+    }
+
+    @Override
+    public void onResponderInitialized(DSLink link) {
+        Node superRoot = link.getNodeManager().getSuperRoot();
+        NodeBuilder builder = superRoot.createChild("example");
+        builder.setDisplayName("Example");
+        builder.setValueType(ValueType.STRING);
+        builder.setValue(new Value("Hello world"));
+        builder.build();
+
+        LOGGER.info("Initialized");
     }
 
     @Override
     public void onResponderConnected(DSLink link) {
         LOGGER.info("Connected");
+    }
 
-        Node superRoot = link.getNodeManager().getSuperRoot();
-        Node example = superRoot.createChild("ExampleNode").build();
-        Value exampleNodeValue = new Value("Hello World!");
-        example.setValueType(exampleNodeValue.getType());
-        example.setValue(exampleNodeValue);
+    public static void main(String[] args) {
+        DSLinkFactory.start(args, new Main());
     }
 }
